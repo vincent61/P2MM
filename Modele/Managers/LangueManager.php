8d8fa84b9c90ead7975fd51985525a3afa8677ad
Vincent Mercier <vincent.p.mercier@gmail.com>
2013-05-07 17:55:39 +0200

    Ajout de la classe Langue et de son Manager

---
+++ Modele/Managers/LangueManager.php
@@ -0,0 +1,77 @@
+﻿<?php
+include '../Modele/ModeleMemoire/Langue.php';
+
+class LangueManager{
+	private $_db; // Instance de db
+ 
+  public function __construct($db)
+  {
+    $this->setDb($db);
+  }
+ 
+  public function add($langue)
+  {
+	if($langue instanceof Langue){
+		$this->_db->exec('INSERT INTO Langue VALUES (\''.$langue->getLangue().'\');');
+	}
+	else
+		throw new Exception('Type reçu erroné.');
+  }
+ 
+  public function delete($langue)
+  {
+	if($langue instanceof Langue){
+		$this->_db->exec('DELETE FROM Langue WHERE langue = \''.$langue->getLangue().'\';');
+	}
+	else
+		throw new Exception('Type reçu erroné.');
+  }
+ 
+  public function get($langue)
+  { 
+	$l = (String) $langue;
+    $q = $this->_db->query('SELECT langue FROM Langue WHERE langue = \''.$langue.'\'');
+    $donnees = $q->fetch(PDO::FETCH_ASSOC);
+    return new Langue($donnees['langue']);
+  }
+  
+  public function getAll()
+  { 
+    $q=$this->_db->prepare("SELECT * FROM Langue order by langue");
+	$q->execute();
+    $donnees = $q->fetchAll();
+    return $donnees;
+  }
+  
+ /*
+  public function getList()
+  {// Serieux soucis de gestion mémoir: taille des array limités
+    $dictionnaire = array();
+ 
+    $q = $this->_db->query('SELECT mot FROM Dictionnaire ORDER BY mot');
+ 
+    while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+    {
+     // $dictionnaire[] = new Mot($donnees['mot']);
+	  echo $donnees['mot'];
+    }
+    return $dictionnaire;
+  }*/
+
+  public function update($oldLangue, $newLangue)
+  {
+	if($oldLangue instanceof Langue and $newLangue instanceof Langue){
+	   $this->_db->exec('UPDATE Langue SET langue = \''.$newLangue->getLangue().'\' WHERE langue = \''.$oldLangue->getLangue().'\';');
+	}
+	else
+		throw new Exception('Type reçu erroné.');
+  }
+ 
+  public function setDb($db)
+  {
+    $this->_db = $db;
+  }
+}
+
+?>
+	
---
+++ Modele/ModeleMemoire/Langue.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+class Langue{
+protected $langue;
+public function __construct($l){
+	$this->langue = $l;	
+}
+public function getLangue(){
+	return $this->langue;
+}
+
+}
+?>
