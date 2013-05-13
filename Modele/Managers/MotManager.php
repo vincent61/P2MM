389dbe5ba5f9ce0df4f2b3185875ab5ca2683c68
Laura Daras <laura.daras@gmail.com>
2013-05-13 19:03:18 +0200

    fonction codage
    * Rajout des polices dans les paramètres
    * Codage dans les polices demandées (toutes celles qui correspondent à
    la casse du mot, par défaut)
    * Fonction exist ajoutée à MotManager

--- Modele/Managers/MotManager.php
+++ Modele/Managers/MotManager.php
@@ -1,6 +1,8 @@
 ﻿<?php
 
 include '../Modele/ModeleMemoire/Mot.php';
+include '../Modele/Managers/LettreManager.php';
+include '../Modele/Managers/PoliceManager.php';
 include '../Modele/Managers/MotCodeManager.php';
 include '../Modele/Managers/CorrespondanceLettreManager.php';
 include '../Modele/Managers/CorrespondanceMotManager.php';
@@ -61,139 +63,176 @@ class MotManager{
 	   throw new Exception('Type reçu erroné.');
   }
  
+	public function exist($mot)
+  { 
+    $q = $this->_db->query('SELECT count(*) AS total FROM Mot WHERE mot = \''.$mot.'\';');
+    $donnees = $q->fetch(PDO::FETCH_ASSOC);
+    return $donnees['total'];
+  }
+ 
   public function setDb($db)
   {
     $this->_db = $db;
   }
   
-  public function codage(Mot $motParam, $policesParam)
+  public function codage(Mot $motParam, array $policesParam=NULL)
   {
   include '../dbconnect.php';
+  $motManager = new MotManager ($con);
   $mot= $motParam->getMot();
   
   $lettreManager= new LettreManager($con);
   $correspLetMan = new CorrespondanceLettreManager($con);
-  $polices=$policesParam;
-  
-  //if (is_null($polices))  
-  		//$polices= array
-		
-  /* Code en Python pour les polices  (a adapter en php)
-  if args:
-			#si des polices sont spécifiées
-			for p in args:
-				try:
-					polices.append(Police.objects.get(police=p, casse=self.casse))
-					#on s'assure que la police correspond à la casse du mot
-				except Police.DoesNotExist, e:
-					print e
-		else:
-			#si aucun argument, on récupère toutes les polices de la même casse que le mot
-			polices= Police.objects.filter(casse=self.casse)
-		*/
-	
-  
-  $suite=true;
-  $listeResultat=array('');
-  for($i=0; $i<strlen($mot); $i++)   // On parcourt chaque lettre du mot initial
-	{	
-		
-	
-		//echo $mot[$i];
-		if ($suite){       // Interet de suite???
-		try                
-		{	
-			$vraieLettre= $lettreManager->get($mot[$i]);  // La lettre en cours doit correspondre à une lettre existante dans la table Lettre de la BDD
-			$codesLettres= $correspLetMan->getCodes($vraieLettre,'min_haut');  // On récupere les codes auxquels correspond la lettre en cours
-			/*foreach($codeslettres as $codelettre)
-				{	
-					echo ($codeslettre);
-				}*/
-				
-			$nbCodesLettre= count($codesLettres);
-			//echo $nbCodeLettre;
-			$tailleResultatIni = count ($listeResultat);
-			
-			if($nbCodesLettre > 1)   // Si la lettre en cours de codage a au moins 2 codes, on duplique le tableau de résultats
-			{
-				for ($ii=0; $ii<($nbCodesLettre-1); $ii++)
-					{
-						for ($r=0; $r<$tailleResultatIni; $r++)
-						{
-							array_push($listeResultat, $listeResultat[$r]);
-						}
-					}
-					/*foreach($listeResultat as $resultat){
-						 echo $resultat;   // Verif des resultats contenus ds la liste des resultats
-					}*/
-			}			
-			$j=0;
-			$k=0;
-			$tailleResultatInter=count($listeResultat);
-			/*echo "taille";
-			echo $tailleResultatInter;*/
-			
-			for ($ii=0; $ii<($tailleResultatInter); $ii++)
-					{ // Exception div/0
-						try {	
-								if ($nbCodesLettre==0)
-									{
-										throw new Exception('Division par zéro.');
-									}
-								else
-									if ($j < $tailleResultatInter/$nbCodesLettre) 
-										{
-											$j++;    // j est un compteur permettant de parcourir chaque partie identique de la liste de resultats
-										}
-										/* exemple: On 3 codes qui correspondent à notre lettre. On aura donc auparavant recopié 2 fois la liste des resultats
-										On obtient une listeResultat composées de 3 parties identiques. Sur chaque partie, on ajoute 1 code différent de la liste de code.
-										*/
-									
-									else {
-											$j=1;
-											$k++;   // k est la variable qui permet d'identifier le code dans la liste codesLettres
-										}
-									
-									//print_r (array_values($codesLettres));
-									// echo ($codesLettres[$k]['code']);
-									$listeResultat[$ii].= ($codesLettres[$k]['code']);
-									//echo $listeResultat[$ii]; 
-									//echo "<br>";
-							 
-							} catch (Exception $e) {
-								$suite=false;
-								echo 'Exception reçue : ',  $e->getMessage(), "\n"; }
-							
-					}
-			
+  $policesManager= new PoliceManager($con);
+  $policetab= array();
+  $inc=0;
+  /*
+  if ($motManager->exist($mot)>0)  // si le mot n'existe pas dans la BDD, on l'ajoute à la table Mot
+		echo "Mot existe déjà en BDD <br>";
+		else
+		{	$motManager->add($motParam);
+			echo "mot ajouté ";
 		}
-		catch (Exception $e)
-		{	
-			$suite=false;
-			throw new Exception('Lettre n\'existe pas!', 0, $e);
+	*/	
+  if (is_null($policesParam))  // Si aucune police spécifiée, on code le mot dans toutes les polices existantes correspondantes à la casse du mot
+  		{	$polices=$policesManager->getAllCasse($motParam->getCasse());
+			foreach($polices as $pol)
+			{			$policetab[$inc]=$pol[0];
+						$inc++;		
+			}	
 		}
+	else	// On prend les polices passées en parametre et on vérifie que la police correspond à la casse du mot (sinon on ne code pas dans la police qui pose problème)
+		{	$incc=0;
+			$policesTotal=$policesManager->getAllCasse($motParam->getCasse());
+			
+			foreach($policesTotal as $policesTot)
+				{ 
+					$polices[$incc]=$policesTot[0];
+					$incc++;
+				}
+			
+			foreach ($policesParam as $pol)
+			{if (in_array($pol,$polices))
+				{	$policetab[$inc]=$pol;
+					$inc++;}
+					else 
+						echo "La police '$pol' n'existe pas, ou n'est pas dans la bonne casse. <br>";
 			}
-		
-  }
+		}	
+  
+  // Codage du mot dans toutes les polices determinées
+  foreach($policetab as $pol)
+  {
+	$listeResultat=array('');
+	$suite=true;
+	$correspMotMan = new CorrespondanceMotManager($con);
+	
+	// Si mot déjà codé dans la police alors stop
+	if ($correspMotMan->existCorrespMot($mot,$pol) > 0)
+		{	//echo $correspMotMan->existCorrespMot($mot,$pol);
+			$suite = false;
+		}
+	
+	if ($suite) // si mot n'a pas été codé dans la police
+	{
+		for($i=0; $i<strlen($mot); $i++)   // On parcourt chaque lettre du mot initial
+		{	
+			//echo $mot[$i];
+			if ($suite)
+		{       // Interet de suite???
+			try                
+			{	
+				try {
+				$vraieLettre= $lettreManager->get($mot[$i]);  // La lettre en cours doit correspondre à une lettre existante dans la table Lettre de la BDD
+				}
+				catch (Exception $e) 
+								{
+									$suite=false;
+									echo 'Exception : ',  $e->getMessage(), "\n"; 
+								}
+				$codesLettres= $correspLetMan->getCodes($vraieLettre, $pol);  // On récupere les codes auxquels correspond la lettre en cours
+				/*foreach($codesLettres as $codelettre)
+					{	
+						echo $codelettre['code'];
+					}*/
+					
+				$nbCodesLettre= count($codesLettres);
+				$tailleResultatIni = count ($listeResultat);
+				
+				if($nbCodesLettre > 1)   // Si la lettre en cours de codage a au moins 2 codes, on duplique le tableau de résultats
+				{
+					for ($ii=0; $ii<($nbCodesLettre-1); $ii++)
+						{
+							for ($r=0; $r<$tailleResultatIni; $r++)
+							{
+								array_push($listeResultat, $listeResultat[$r]);
+							}
+						}
+						/*foreach($listeResultat as $resultat){
+							 echo $resultat;   // Verif des resultats contenus ds la liste des resultats
+						}*/
+				}			
+				$j=0;
+				$k=0;
+				$tailleResultatInter=count($listeResultat);
+				
+				for ($ii=0; $ii<($tailleResultatInter); $ii++)
+						{ // Exception div/0
+							try {	
+									if ($nbCodesLettre==0)
+										{
+											throw new Exception('Division par zéro.');
+										}
+									else
+										if ($j < $tailleResultatInter/$nbCodesLettre) 
+											{
+												$j++;    // j est un compteur permettant de parcourir chaque partie identique de la liste de resultats
+											}
+											/* exemple: On 3 codes qui correspondent à notre lettre. On aura donc auparavant recopié 2 fois la liste des resultats
+											On obtient une listeResultat composées de 3 parties identiques. Sur chaque partie, on ajoute 1 code différent de la liste de code.
+											*/
+										
+										else{
+												$j=1;
+												$k++;   // k est la variable qui permet d'identifier le code dans la liste codesLettres
+											}
+										
+										//print_r (array_values($codesLettres));
+										$listeResultat[$ii].= ($codesLettres[$k]['code']);
+										//echo $listeResultat[$ii]; 
+								 
+								} catch (Exception $e) 
+								{
+									$suite=false;
+									echo 'Exception reçue : ',  $e->getMessage(), "\n"; 
+								}
+						}	
+			}
+			catch (Exception $e)
+			{	$suite=false;
+				throw new Exception('Lettre n\'existe pas!', 0, $e);
+			}
+		}}
+	} 
   // Ajout des résultats dans la table MotCode
   if ($suite==true)
   {
 	  for ($i=0; $i<count($listeResultat); $i++)
 	{
-		  echo $listeResultat[$i];
-		  echo "<br>";
-		  $motCode = new MotCode( $listeResultat[$i], "min_haut");
+		  //echo $listeResultat[$i];
+		  //echo "<br>";
+		  $motCode = new MotCode( $listeResultat[$i], $pol);
 		  $motCodeManager = new MotCodeManager($con);
 		  $motCodeManager->add($motCode);
 		  
 		  // Ajout de la correspondance Mot - MotCode pour chaque MotCode
-		  $correspMot = new CorrespondanceMot($mot, $motCode->getCode(), "min_haut");
+		  $correspMot = new CorrespondanceMot($mot, $motCode->getCode(), $pol);
 		  $correspMotMan = new CorrespondanceMotManager($con);
 		  $correspMotMan->add($correspMot);
 	}	  
-	}
-  else echo "Aucun mot inséré car division par zéro";
- }
+  }
+  else echo "Aucun mot inséré <br>";
+ }}
 }
 
 ?>
