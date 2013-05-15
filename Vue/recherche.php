e4253c9d8bee4c4f939490a20e6e56716436d4ac
Maeva G <guerryma.utc@gmail.com>
2013-05-15 12:03:08 +0200

    suite

---
+++ Vue/recherche.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title> Recherche de Combinaisons </title>
+	</head>
+
+	<body>
+		<? include "base/header.html"; ?>
+		<? include "base/navBar.html"; ?>
+		<div id="recherche">
+		</div>
+		<div id="resultats">
+		</div>
+		
+		<menu id="menuRech">
+			<a href="#recherche"> Recherche </a>
+			<a href="#resultats"> Résultats </a> 
+		</menu>
+
+		<? include "base/footer.html"; ?>
+	</body>
+</html>
