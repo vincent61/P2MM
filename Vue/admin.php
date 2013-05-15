e43f26b1b43fc8cdea5029be7daa571efafd9eb1
Vincent Mercier <vincent.p.mercier@gmail.com>
2013-05-15 12:09:46 +0200

    Admin Suite

--- Vue/admin.php
+++ Vue/admin.php
@@ -1,13 +1,24 @@
 <?php
 // Ne doit plus servir qu'à appeler une vue !!!! 
 ?>
+<!DOCTYPE html>
+<html>
 
-<p><a href="Controleurs/lettre.php">Lettre</a></p>
-<p><a href="Controleurs/dictionnaire.php">Dictionnaire</a></p>
-<p><a href="Controleurs/police.php">Police</a></p>
-<p><a href="Controleurs/langue.php">Langues</a></p>
-<p><a href="Controleurs/codelettre.php">Code lettre</a></p>
-<p><a href="Controleurs/mot.php">Mot</a></p>
-<p><a href="Controleurs/motcode.php">Mot codes</a></p>
+	<head>
+		<meta charset="utf-8" />
+		<title> Espace d'administration </title>
+	</head>
 
-
+	<body>
+		<? include "base/header.html"; ?>
+        <? include "base/navBar.html"; ?>
+        <p><a href="Controleurs/lettre.php">Lettre</a></p>
+        <p><a href="Controleurs/dictionnaire.php">Dictionnaire</a></p>
+        <p><a href="Controleurs/police.php">Police</a></p>
+        <p><a href="Controleurs/langue.php">Langues</a></p>
+        <p><a href="Controleurs/codelettre.php">Code lettre</a></p>
+        <p><a href="Controleurs/mot.php">Mot</a></p>
+        <p><a href="Controleurs/motcode.php">Mot codes</a></p>
+        <? include "base/footer.html"; ?>
+	</body>
+</html>
