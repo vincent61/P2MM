126593cf29c341570189b4a243a1842569e6676d
Vincent Mercier <vincent.p.mercier@gmail.com>
2013-05-15 12:03:10 +0200

    Suite Squelette html

---
+++ Vue/admin.php
@@ -0,0 +1,13 @@
+<?php
+// Ne doit plus servir qu'à appeler une vue !!!! 
+?>
+
+<p><a href="Controleurs/lettre.php">Lettre</a></p>
+<p><a href="Controleurs/dictionnaire.php">Dictionnaire</a></p>
+<p><a href="Controleurs/police.php">Police</a></p>
+<p><a href="Controleurs/langue.php">Langues</a></p>
+<p><a href="Controleurs/codelettre.php">Code lettre</a></p>
+<p><a href="Controleurs/mot.php">Mot</a></p>
+<p><a href="Controleurs/motcode.php">Mot codes</a></p>
+
+
