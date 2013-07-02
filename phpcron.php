f53fa0b6304acf3d0e328c61a7d8cd212e0b7a12
Maeva G <guerryma.utc@gmail.com>
2013-07-02 23:58:15 +0200

    cron test bis

--- phpcron.php
+++ phpcron.php
@@ -1,3 +1,8 @@
-<?php $output = shell_exec('crontab -l');
+<?php
+$output = shell_exec('crontab -l');
+file_put_contents('./tmp/crontab.txt', $output.'* * * * * NEW_CRON'.PHP_EOL);
+echo exec('crontab /tmp/crontab.txt');
+
+$output = shell_exec('crontab -l');
 echo $output;
 ?>
