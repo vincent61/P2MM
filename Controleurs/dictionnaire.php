ba7e5703739027ee906a49a76c6131a495b3e191
Maeva G <guerryma.utc@gmail.com>
2013-09-03 14:46:46 +0200

    Revert "dictionnaire : mise à jour des includes"
    This reverts commit 2778fd1cf8e019ac3a7805ed1f19a9fff6b598d6.

--- Controleurs/dictionnaire.php
+++ Controleurs/dictionnaire.php
@@ -1,8 +1,10 @@
 ﻿<?php
 ini_set("auto_detect_line_endings", true);
+include_once '../dbconnect.php';
+include_once '../cheminsPerso.php';
 //include_once 'dictionnaireTools.php';
-include_once 'modele/Managers/DictionnaireManager.php';
-include_once 'modele/Managers/LangueManager.php';
+include_once '../modele/Managers/DictionnaireManager.php';
+include_once '../modele/Managers/LangueManager.php';
 $dictionnaireManager = new DictionnaireManager($con);
 $langueManager = new LangueManager($con);
 
@@ -84,5 +86,5 @@ else
 }
 $langues = $langueManager->getAll();
 //On inclue la vue
-include 'vue/dictionnaire.php'; 
+include '../vue/dictionnaire.php'; 
 ?></em></em>
